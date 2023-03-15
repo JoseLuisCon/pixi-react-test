d1164fee1e33fe21fcd91b32b6460d2a1201fbfb
JoseLuisCon <condepa_72@icloud.com>
2023-03-15 16:59:55 +0100

    Punto retorno cambio diseño

--- src/components/AnimatedSprite.jsx
+++ src/components/AnimatedSprite.jsx
@@ -2,6 +2,8 @@ import React, {useState, useEffect} from 'react'
 
 import * as PIXI from 'pixi.js'
 
+
+
 import {
   Container,
   AnimatedSprite,
@@ -10,8 +12,8 @@ import {
 } from '@inlet/react-pixi';
 
 
-const spritesheet =
-  "https://pixijs.io/examples/examples/assets/spritesheet/fighter.json";
+// const spritesheet = "https://pixijs.io/examples/examples/assets/spritesheet/fighter.json";
+const spritesheet = "spritesheet.json";
 
 
 export const JetSpriteAnimated = () => {
@@ -20,30 +22,37 @@ export const JetSpriteAnimated = () => {
   const [rot, setRot] = useState(0);
   const app = useApp();
 
-  useTick(delta => setRot(r => r + (0.01 * delta)));
+  // useTick(delta => setRot(r => r + (0.01 * delta)));
 
   // load
   useEffect(() => {
+   
     app.loader.add(spritesheet).load((_, resource) => {
       setFrames(
         Object.keys(resource[spritesheet].data.frames).map(frame =>
           PIXI.Texture.from(frame)
-        )
-      );
-    });
+          )
+          );
+        });
+
+
+
+        
+        
   }, []);
 
   if (frames.length === 0) {
     return null;
   }
-
+  
   return (
     <Container rotation={rot} x={1200} y={350}>
       <AnimatedSprite
-        animationSpeed={0.5}
+        animationSpeed={0.25}
         isPlaying={true}
         textures={frames}
         anchor={0.5}
+        
       />
     </Container>
   )
